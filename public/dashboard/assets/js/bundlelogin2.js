b06d6d98da5993939079ec31b0c51773b2bde9d0
FallenGravity <ethanvb55@gmail.com>
2019-04-02 09:34:51 +0200

    Added a modal, Made UI changes

--- public/dashboard/assets/js/bundlelogin2.js
+++ public/dashboard/assets/js/bundlelogin2.js
@@ -15,7 +15,7 @@ if (typeof web3 !== 'undefined')
       else if (accounts.length == 0){
            console.log("User is not logged in");
            var formText = '<form id="subscribe-form"><div class="form-group group-inline"></div><p class="m-b-0 fade-color-3"><small>No account</small></p></form>';
-           document.getElementById("ethoFSLogin2").innerHTML = formText;
+           document.getElementById("ethoFSLogin3").innerHTML = formText;
       }
       else{
            console.log("User is logged in");
@@ -34,13 +34,13 @@ if (typeof web3 !== 'undefined')
                        if(result)
                        {
                            console.log("User Account Found: " + web3.eth.defaultAccount);
-                           var formText = result ;
-                           document.getElementById("ethoFSLogin2").innerHTML = formText;
+                           var formText = '<h1>Welcome Back ' + result + '</p><p>User Address: ' + GlobalUserAddress + '</h1>';
+                           document.getElementById("ethoFSLogin3").innerHTML = formText;
                        }
                        }else{
                            console.log("Error getting user account name");
                            var formText = '<form id="subscribe-form"><div class="form-group group-inline"></div><p class="m-b-0 fade-color-3"><small>No account</small></p></form>';
-                           document.getElementById("ethoFSLogin2").innerHTML = formText;
+                           document.getElementById("ethoFSLogin3").innerHTML = formText;
                        }
                    });
 
@@ -48,14 +48,14 @@ if (typeof web3 !== 'undefined')
                {
                    console.log("User Account Not Found: " + web3.eth.defaultAccount);
                    var formText = '<form id="signup-form"><div class="form-group group-inline"><label for="etho-address" class="form-label">Enter New ethoFS User Name</label><br><input id="etho-address" class="form-control form-primary form-animated" name="etho-address" required="" placeholder=""><button type="button" class="btn btn-primary" onclick="window.AddNewUser();">Signup</button></div><p class="m-b-0 fade-color-3"><small><span id="wait">Click Signup to Register Your Metamask/Ether-1 Browser Extension Address With ethoFS To Continue</span></small></p></form>';
-                   document.getElementById("ethoFSLogin2").innerHTML = formText;
+                   document.getElementById("ethoFSLogin3").innerHTML = formText;
                }
            }
            else
            {
                console.log("Access To Browser Extension Not Found");
                var formText = '<form id="subscribe-form"><div class="form-group group-inline"></div><p class="m-b-0 fade-color-3"><small>No account</small></p></form>';
-               document.getElementById("ethoFSLogin2").innerHTML = formText;
+               document.getElementById("ethoFSLogin3").innerHTML = formText;
            }
        });
 }
@@ -65,7 +65,7 @@ else
 {
     console.log("Access To Browser Extension Not Found");
     var formText = '<form id="subscribe-form"><div class="form-group group-inline"></div><p class="m-b-0 fade-color-3"><small>No account</small></p></form>';
-    document.getElementById("ethoFSLogin2").innerHTML = formText;
+    document.getElementById("ethoFSLogin3").innerHTML = formText;
 }
 })
 
@@ -85,7 +85,7 @@ window.AddNewUser = function(){
                     console.log("Transaction Has Been Mined: " + receipt);
                     console.log("User Account Found: " + web3.eth.defaultAccount);
                     var formText = '<a href="dashboard/index.html"><button type="enter" id="enter-button" class="btn btn-primary-enter">Enter</button></a><br><p class="m-b-0 fade-color-3"><small>User Address Found - Click Enter To Proceed</small></p>';
-                    document.getElementById("ethoFSLogin2").innerHTML = formText;
+                    document.getElementById("ethoFSLogin3").innerHTML = formText;
                 });
             }else{
                 console.log("There was a problem adding new user");
